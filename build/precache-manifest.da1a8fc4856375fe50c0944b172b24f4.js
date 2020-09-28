380b42beb641ef26afbbcef0cedfa47d9be67a9e
carban <murillo.carlos@correounivalle.edu.co>
2020-09-28 17:57:30 -0500

    init

---
+++ build/precache-manifest.da1a8fc4856375fe50c0944b172b24f4.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddfe8782a29242c7ad38e52dd8ccfdbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59801de464c3a6f643ff",
+    "url": "/static/css/2.151d002d.chunk.css"
+  },
+  {
+    "revision": "1e03f1b70d4c738a76f4",
+    "url": "/static/css/main.da413cb3.chunk.css"
+  },
+  {
+    "revision": "59801de464c3a6f643ff",
+    "url": "/static/js/2.a5fd3dd6.chunk.js"
+  },
+  {
+    "revision": "1e03f1b70d4c738a76f4",
+    "url": "/static/js/main.e719dd0b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8482c9cfcdfcf6048300c76005696053",
+    "url": "/static/media/count.8482c9cf.png"
+  },
+  {
+    "revision": "8d299c8b956aeb1f23c60d604596bca6",
+    "url": "/static/media/countInactive.8d299c8b.png"
+  },
+  {
+    "revision": "e21327386c16b1635d47a8a5deff0dec",
+    "url": "/static/media/england_icon.e2132738.png"
+  },
+  {
+    "revision": "c258a6dff0016b6b01dcf685846270cc",
+    "url": "/static/media/logo.c258a6df.png"
+  },
+  {
+    "revision": "ef1b601866e9766b294914c4f95da16a",
+    "url": "/static/media/marker.ef1b6018.png"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "7e3f911493401f9cb6962a788a80ae22",
+    "url": "/static/media/portuguese_icon.7e3f9114.png"
+  },
+  {
+    "revision": "9c933e20fa2a94babf6ad27303a4bcdd",
+    "url": "/static/media/spain_icon.9c933e20.png"
+  },
+  {
+    "revision": "d0ee15003917ba28f335b8286e7171e9",
+    "url": "/static/media/tesla.d0ee1500.png"
+  }
+]);
